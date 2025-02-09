0ec7f69307592fed85dcb889b66d2e55869e91ac
hugohenrick <hugohenrick16@gmail.com>
2025-02-09 17:17:58 -0300

    fix: ajsute cors

--- backend/internal/delivery/http/middleware/cors.go
+++ backend/internal/delivery/http/middleware/cors.go
@@ -7,7 +7,17 @@ import (
 
 func CORS() gin.HandlerFunc {
 	config := cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"http://localhost:3000",
+			"https://comunidade-git-main-hugohenricks-projects.vercel.app",
+			"https://comunidade-three.vercel.app",
+			"https://comunidade-hugohenricks-projects.vercel.app",
+			"https://comunidademais.app",
+			"https://www.comunidademais.app",
+			"https://comunidademais.com",
+			"https://www.comunidademais.com",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		AllowCredentials: true,
